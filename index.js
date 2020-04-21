5c9a582a1753f6a088f8af2fa0ed853babba3222
Kwaku-Boateng Joshua <Joshuakwaku@outlook.com>
2020-04-21 21:43:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./components');
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 8000;
 
 // process.on('uncaughtException', function (err) {
 //     logger.info((new Date).toUTCString() + ' uncaughtException:', err.message);
